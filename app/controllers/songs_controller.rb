1dbf9e9f4645f5e29fdebd3b350a052dde2ba8f9
taroj khan <tarujkhan@yahoo.com>
2021-02-14 18:45:22 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
+      @songs = Artist.find(params[:artist_id])
     else
     @songs = Song.all
   end
